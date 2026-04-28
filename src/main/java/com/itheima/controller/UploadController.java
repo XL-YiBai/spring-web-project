dddda3e01904748b4d90f5602576cf51d7409636
XL-YiBai <lei.xia@emqx.io>
2026-04-29 00:44:39 +0800

    feat: 文件上传-本地

---
+++ src/main/java/com/itheima/controller/UploadController.java
@@ -0,0 +1,29 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class UploadController {
+
+    @PostMapping("/upload")
+    public Result upload(String name, Integer age, MultipartFile file) throws IOException {
+        log.info("接收参数：{},{},{}", name, age, file);
+        // 获取原始的文件名
+        String originalFilename = file.getOriginalFilename();
+
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String newFileName = UUID.randomUUID().toString() + extension;
+        // 保存文件
+        file.transferTo(new File("/Users/xialei/Desktop/后端/java learning/web-ai-project/file/" + newFileName));
+        return Result.success();
+    }
+}
--- src/main/java/com/itheima/service/impl/EmpServiceImpl.java
+++ src/main/java/com/itheima/service/impl/EmpServiceImpl.java
@@ -4,14 +4,13 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.itheima.mapper.EmpExprMapper;
 import com.itheima.mapper.EmpMapper;
-import com.itheima.pojo.Emp;
-import com.itheima.pojo.EmpExpr;
-import com.itheima.pojo.EmpQueryParam;
-import com.itheima.pojo.PageResult;
+import com.itheima.pojo.*;
+import com.itheima.service.EmpLogService;
 import com.itheima.service.EmpService;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDate;
@@ -27,11 +26,14 @@ public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpExprMapper empExprMapper;
 
-    /**
-     * 原始分页查询
-     * @param page 页码
-     * @param pageSize 每页记录数
-     */
+    @Autowired
+    private EmpLogService empLogService;
+
+//    /**
+//     * 原始分页查询
+//     * @param page 页码
+//     * @param pageSize 每页记录数
+//     */
 //    @Override
 //    public PageResult<Emp> page(Integer page, Integer pageSize) {
 //        // 1. 调用 Mapper 接口查询总记录数
@@ -45,14 +47,14 @@ public class EmpServiceImpl implements EmpService {
 //        return new PageResult<Emp>(total, rows);
 //    }
 
-    /**
-     * 使用 PageHelper 插件分页查询
-     * @param page 页码
-     * @param pageSize 每页记录数
-     * 注意事项：
-     *                 1. 使用 PageHelper 时定义的 SQL 语法结尾不能加分号；不然 PageHelper 在后面拼接 limit 语句执行会报错
-     *                 2. PageHelper 仅仅能对紧跟在其后的第一个查询语句进行分页操作
-     */
+//    /**
+//     * 使用 PageHelper 插件分页查询
+//     * @param page 页码
+//     * @param pageSize 每页记录数
+//     * 注意事项：
+//     *                 1. 使用 PageHelper 时定义的 SQL 语法结尾不能加分号；不然 PageHelper 在后面拼接 limit 语句执行会报错
+//     *                 2. PageHelper 仅仅能对紧跟在其后的第一个查询语句进行分页操作
+//     */
 //    @Override
 //    public PageResult<Emp> page(Integer page, Integer pageSize, String name, Integer gender, LocalDate begin, LocalDate end) {
 //        // 1. 在查询之前，先设置分页参数（PageHelper）
@@ -79,22 +81,32 @@ public class EmpServiceImpl implements EmpService {
         return new PageResult<Emp>(p.getTotal(), p.getResult());
     }
 
+    // 事务管理的注解 - 默认出现运行时异常 RuntimeException 才会回滚
+    // 可以通过 rollbackFor 这个数组声明哪些异常要回归，这里使用 Exception.class 代表所有异常
+    @Transactional(rollbackFor = {Exception.class})
     @Override
     public void save(Emp emp) {
-        // 1. 保存员工的基本信息
-        emp.setCreateTime(LocalDateTime.now());
-        emp.setUpdateTime(LocalDateTime.now());
-        System.out.println("@@@@" + emp);
-        empMapper.insert(emp);
+        try {
+            // 1. 保存员工的基本信息
+            emp.setCreateTime(LocalDateTime.now());
+            emp.setUpdateTime(LocalDateTime.now());
+            empMapper.insert(emp);
 
-        // 2. 保存员工的工作经历信息
-        List<EmpExpr> exprList = emp.getExprList();
-        if (!CollectionUtils.isEmpty(exprList)) {
-            // 遍历集合，为 empId 赋值
-            exprList.forEach(empExpr -> {
-                empExpr.setEmpId(emp.getId());
-            });
-            empExprMapper.insertBatch(exprList);
+            // 2. 保存员工的工作经历信息
+            List<EmpExpr> exprList = emp.getExprList();
+            if (!CollectionUtils.isEmpty(exprList)) {
+                // 遍历集合，为 empId 赋值
+                exprList.forEach(empExpr -> {
+                    empExpr.setEmpId(emp.getId());
+                });
+                empExprMapper.insertBatch(exprList);
+            }
+        } finally {
+            // 记录操作日志
+            EmpLog empLog = new EmpLog(null, LocalDateTime.now(), "新增员工：" + emp);
+            // 为了让操作失败时也能记录日志，该方法使用 @Transactional(propagation = Propagation.REQUIRES_NEW) 注解
+            // 代表需要在一个新的事物中运行，与当前方法的事务隔离
+            empLogService.insertLog(empLog);
         }
     }
 }
